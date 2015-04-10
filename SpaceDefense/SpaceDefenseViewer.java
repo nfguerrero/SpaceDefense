c9813101313a56f2abc16dc648876b88b3c6e53e
Nicolas Guerrero <nfguerrero@NN123-HA-14.HS.sd203.org>
2015-04-10 15:10:03 -0500

    initial stuff

---
+++ SpaceDefense/SpaceDefenseViewer.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+/**
+ * Defend against enemies and protect the civilians
+ * 
+ * @nfguerrero 
+ * @ver. 1
+ */
+public class SpaceDefenseViewer
+{
+    /**
+     * Default constructor for objects of class SpaceDefenseViewer
+     */
+    public SpaceDefenseViewer()
+    {
+    }
+}
